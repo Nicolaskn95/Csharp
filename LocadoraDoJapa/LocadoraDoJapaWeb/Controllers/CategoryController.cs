24b545ca0d2b06dafce3bd29c5cf2e33b8a1d1ac
Nicolas Nagano <nicolasjap@hotmail.com>
2023-05-23 15:12:10 -0300

    Initial_Project.NET7

---
+++ LocadoraDoJapa/LocadoraDoJapaWeb/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using LocadoraDoJapaWeb.Data;
+using LocadoraDoJapaWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDoJapaWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //linha abaixo é um implementação do .NetCore diferente do Legacy
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ LocadoraDoJapa/LocadoraDoJapaWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LocadoraDoJapaWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display order")]
+        public int DisplayOrder { get; set; }
+    }
+}
